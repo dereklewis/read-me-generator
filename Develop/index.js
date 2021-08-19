a55c51b9b007e2a88e626096be4946e3cbae68d3
Derek Lewis <derek.lewis.den@outlook.com>
2021-08-19 16:13:34 -0600

    added the questions for the prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,58 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer")
+const fs = require("fs");
+const Choices = require("inquirer/lib/objects/choices");
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is your projects name?",
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Give a brief description of your project",
 
-// TODO: Create a function to initialize app
-function init() {}
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What kind of license does your project use?",
+        choices: ["MIT", "Other"],
+    },
+    {
+        type: ,
+        name: ,
+        message: ,
+    }
+    
+];
 
-// Function call to initialize app
-init();
+inquirer.prompt(questions).then((data) => {
+    console.log(data);
+})
+
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
---
+++ README.md
@@ -0,0 +1,6 @@
+# READ-ME-Generator
+
+build a command line application that accepts user input and generates a readme file
+
+tech used - inquirer, fs, js
+
